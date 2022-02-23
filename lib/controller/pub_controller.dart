9914f1e768861d05ab46d57016ab09d2c6a50133
Rexios <rexios@rexios.dev>
2022-02-22 20:16:20 -0500

    Use pub_api_client to fetch publisher packages

--- lib/controller/pub_controller.dart
+++ lib/controller/pub_controller.dart
@@ -7,7 +7,7 @@ import 'package:rexios_dev/model/package_score_info.dart';
 import 'package:rexios_dev/service/github_service.dart';
 
 class PubController {
-  final _pubClient = PubClient(pubUrl: 'https://proxy.rexios.dev/pub');
+  final _pub = PubClient(pubUrl: 'https://proxy.rexios.dev/pub');
   final _github = GetIt.I<GitHubService>();
 
   final packageScoreInfos = RxList<PackageScoreInfo>();
@@ -17,55 +17,35 @@ class PubController {
   }
 
   void _init() async {
-    final rexiosPackages = await _getPackages(publisher: 'rexios.dev');
-    final vrchatPackages = await _getPackages(publisher: 'vrchat.community');
-    final infos = await _getMetricsInfos(
-      packages: rexiosPackages + vrchatPackages,
-    );
+    final rexiosPackages = await _pub.fetchPublisherPackages('rexios.dev');
+    final vrchatPackages =
+        await _pub.fetchPublisherPackages('vrchat.community');
+    final infos =
+        await _getMetricsInfos(results: rexiosPackages + vrchatPackages);
     packageScoreInfos.clear();
     packageScoreInfos.addAll(infos);
   }
 
-  Future<List<String>> _getPackages({
-    required String publisher,
-    int page = 1,
-  }) async {
-    final results =
-        await _pubClient.search('', publisher: publisher, page: page);
-    final packages = results.packages.map((e) => e.package).toList();
-    if (packages.isEmpty) {
-      // Terminating condition
-      // There were no results for this page
-      return [];
-    } else {
-      return packages +
-          await _getPackages(
-            publisher: publisher,
-            page: page + 1,
-          );
-    }
-  }
-
   Future<List<PackageScoreInfo>> _getMetricsInfos({
-    required List<String> packages,
+    required List<PackageResult> results,
   }) async {
-    final scoreCards = <PackageScoreInfo>[];
-    for (final package in packages) {
-      final options = await _pubClient.packageOptions(package);
+    final infos = <PackageScoreInfo>[];
+    for (final result in results) {
+      final options = await _pub.packageOptions(result.package);
 
       // Don't show unlisted or discontinued packages
       if (options.isUnlisted || options.isDiscontinued) {
         continue;
       }
 
-      final score = await _pubClient.packageScore(package);
-      final info = await _pubClient.packageInfo(package);
+      final score = await _pub.packageScore(result.package);
+      final info = await _pub.packageInfo(result.package);
       final stars = await _getStars(info);
-      scoreCards.add(PackageScoreInfo(score: score, info: info, stars: stars));
+      infos.add(PackageScoreInfo(score: score, info: info, stars: stars));
     }
 
     // Sort by popularity
-    return scoreCards.sorted(
+    return infos.sorted(
       (a, b) =>
           b.score.popularityScore?.compareTo(a.score.popularityScore ?? -1) ??
           -1,
